cdb73a841d9996f26276ba7292f520dcbe6933c3
Alice Huang <aliceh4@illinois.edu>
2019-12-06 00:37:26 -0600

    Working Buttons

--- app/src/main/java/com/example/budgetapp/Balance.java
+++ app/src/main/java/com/example/budgetapp/Balance.java
@@ -1,10 +1,15 @@
 package com.example.budgetapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public final class Balance extends AppCompatActivity {
@@ -15,20 +20,94 @@ public final class Balance extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
 
+        Intent intent = getIntent();
+        TextView mealBalance = findViewById(R.id.mealBalance);
+        mealBalance.setText(intent.getStringExtra("meals"));
+        TextView creditBalance = findViewById(R.id.creditBalance);
+        creditBalance.setText(intent.getStringExtra("credits"));
+
+        TextView monthlyBalance = findViewById(R.id.monthlyBalance);
+        monthlyBalance.setText(intent.getStringExtra("monthlyExpense"));
+
+
         Button subtractMeal = findViewById(R.id.mealSpent);
         Button subtractCredit = findViewById(R.id.subtractCredits);
+        Button subtractMonthly = findViewById(R.id.subtractMonthly);
 
         subtractMeal.setOnClickListener(unused -> subtractMealClicked());
         subtractCredit.setOnClickListener(unused -> subtractCreditClicked());
+        subtractMonthly.setOnClickListener(unused -> subtractMonthlyClicked());
     }
 
     private void subtractMealClicked() {
         TextView mealBalance = findViewById(R.id.mealBalance);
-        mealBalance.setText("100");
+        int og = Integer.parseInt(mealBalance.getText().toString());
+        if (og == 0) {
+            showSecondAlertDialog();
+        } else {
+            String after = String.valueOf(og - 1);
+            mealBalance.setText(after);
+        }
     }
 
     private void subtractCreditClicked() {
         TextView creditBalance = findViewById(R.id.creditBalance);
-        creditBalance.setText("100");
+        EditText creditsSpent = findViewById(R.id.creditsSpent);
+        int subtract = Integer.parseInt(creditsSpent.getText().toString());
+        int og = Integer.parseInt(creditBalance.getText().toString());
+        int after = og - subtract;
+        if (after >= 0 && og > 0) {
+            creditBalance.setText(String.valueOf(after));
+            creditsSpent.setText("");
+        } else {
+            showAlertDialog();
+            creditsSpent.setText("");
+        }
+    }
+
+    private void subtractMonthlyClicked() {
+        TextView creditBalance = findViewById(R.id.monthlyBalance);
+        EditText creditsSpent = findViewById(R.id.monthlySpent);
+        int subtract = Integer.parseInt(creditsSpent.getText().toString());
+        int og = Integer.parseInt(creditBalance.getText().toString());
+        int after = og - subtract;
+        if (after >= 0 && og > 0) {
+            creditBalance.setText(String.valueOf(after));
+            creditsSpent.setText("");
+        } else {
+            showAlertDialog();
+            creditsSpent.setText("");
+        }
+    }
+
+    AlertDialog myDialog;
+    public void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Whoops");
+        builder.setMessage("This input is too much! Consider getting something cheaper.");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog = builder.show();
+        return;
+
+    }
+    AlertDialog second;
+    public void showSecondAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Uh oh...");
+        builder.setMessage("You have no more meal swipes.");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                second.dismiss();
+            }
+        });
+        second = builder.show();
+        return;
+
     }
 }
--- app/src/main/java/com/example/budgetapp/MainActivity.java
+++ app/src/main/java/com/example/budgetapp/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,19 +16,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mealplan_planning);
 
-        Button mealPlanOne = findViewById(R.id.twelveTen);
-        Button mealPlanTwo = findViewById(R.id.tenFortyFive);
-        Button mealPlanThree = findViewById(R.id.meals);
-        Button mealPlanFour = findViewById(R.id.credits);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        EditText monthlyExpense = findViewById(R.id.monthlyExpense);
 
-        mealPlanOne.setOnClickListener(unused -> createMealBalanceClicked());
-        mealPlanTwo.setOnClickListener(unused -> createMealBalanceClicked());
-        mealPlanThree.setOnClickListener(unused -> createMealBalanceClicked());
-        mealPlanFour.setOnClickListener(unused -> createMealBalanceClicked());
+        RadioButton mealPlanOne = radioGroup.findViewById(R.id.twelveFifteen);
+        RadioButton mealPlanTwo = radioGroup.findViewById(R.id.tenFortyFive);
+        RadioButton mealPlanThree = radioGroup.findViewById(R.id.meals);
+        RadioButton mealPlanFour = radioGroup.findViewById(R.id.credits);
+
+        Button expenseButton = findViewById(R.id.expenseButton);
+
+        expenseButton.setOnClickListener(unused -> createMealBalanceClicked(radioGroup, monthlyExpense));
     }
 
-    private void createMealBalanceClicked() {
+    private void createMealBalanceClicked(RadioGroup radioGroup, EditText monthlyExpense) {
         Intent intent = new Intent(this, Balance.class);
+        if (radioGroup.getCheckedRadioButtonId() == R.id.twelveFifteen) {
+            intent.putExtra("meals", "12");
+            intent.putExtra("credits", "15");
+        } else if (radioGroup.getCheckedRadioButtonId() == R.id.tenFortyFive) {
+            intent.putExtra("meals", "10");
+            intent.putExtra("credits", "45");
+        } else if (radioGroup.getCheckedRadioButtonId() == R.id.meals) {
+            intent.putExtra("meals", "47");
+            intent.putExtra("credits", "0");
+        } else if (radioGroup.getCheckedRadioButtonId() == R.id.credits) {
+            intent.putExtra("meals", "0");
+            intent.putExtra("credits", "130");
+        } else {
+            intent.putExtra("meals", "0");
+            intent.putExtra("credits", "0");
+        }
+        if (monthlyExpense.getText().toString().equals("")) {
+            intent.putExtra("monthlyExpense", "0");
+        } else {
+            intent.putExtra("monthlyExpense", monthlyExpense.getText().toString());
+        }
         startActivity(intent);
     }
 
